2c87b226138e39e1c6687d50e027c2d7def4b03d
joacoraca <114175765+joacoraca@users.noreply.github.com>
2025-04-10 09:00:08 -0300

    Add files via upload

---
+++ Codigos/ejercicios malloc y punteros.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void imprimirNum (int *cant, int *num){
+    printf("\nLos números ingresados son:\n");
+    for (int i = 0; i < cant; i++) {
+        printf("%d ", num[i]);
+    }
+}
+
+int main() {
+    int *numeros;
+    int cantidad, i;
+    printf("Ingresá la cantidad de numeros que quieras ingresar ");
+    scanf("%d", &cantidad);
+    numeros = (int *) malloc(cantidad * sizeof(int));
+    if (numeros == NULL) {
+        printf("No se pudo asignar memoria.\n");
+        return 1;
+    }
+    for (i = 0; i < cantidad; i++) {
+        printf("Número %d: ", i+1);
+        scanf("%d", &numeros[i]);
+    }
+    imprimirNum (&cantidad, &numeros);
+    free(numeros);
+    return 0;
+}
