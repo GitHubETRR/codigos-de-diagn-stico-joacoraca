539cec430f5a71835595d3de4e243e83adc07e17
joacoraca <114175765+joacoraca@users.noreply.github.com>
2025-05-15 08:48:13 -0300

    Update ejercicio de practica lista simplemente enlazada.c

--- Codigos/ejercicio de practica lista simplemente enlazada.c
+++ Codigos/ejercicio de practica lista simplemente enlazada.c
@@ -6,22 +6,38 @@ struct Nodo {
     struct Nodo* siguiente;
 };
 
+struct Nodo* agregarAlFinal(struct Nodo* apuntaInicio, int valor);
+void mostrarLista(struct Nodo* apuntaInicio);
+void liberarLista(struct Nodo* apuntaInicio);
+
+int main() {
+    struct Nodo* lista = NULL;
+    int valor, opcion;
+    do {
+        printf("Ingresá un número: ");
+        scanf("%d", &valor);
+        lista = agregarAlFinal(lista, valor);
+        printf("Querés agregar otro número? si (1)/no (2): ");
+        scanf(" %d ", &opcion);  
+    } while (opcion == 1 );
+    mostrarLista(lista);
+    liberarLista(lista);
+    return 0;
+}
+
 struct Nodo* agregarAlFinal(struct Nodo* apuntaInicio, int valor) {
     struct Nodo* nuevo = (struct Nodo*) malloc(sizeof(struct Nodo));
     nuevo->dato = valor;
     nuevo->siguiente = NULL;
-
     if (apuntaInicio == NULL) {
         printf("No hay ninguna lista en la que se haya guardado algo \n");
         return nuevo;
     }
-
     struct Nodo* actual = apuntaInicio;
     while (actual->siguiente != NULL) {
         actual = actual->siguiente;
     }
     actual->siguiente = nuevo;
-
     return apuntaInicio;
 }
 
@@ -43,17 +59,3 @@ void liberarLista(struct Nodo* apuntaInicio) {
         free(temp);
     }
 }
-
-int main() {
-    struct Nodo* lista = NULL;
-    int valor, opcion;
-    do {
-        printf("Ingresá un número: ");
-        scanf("%d", &valor);
-        lista = agregarAlFinal(lista, valor);
-        printf("Querés agregar otro número? si (1)/no (2): ");
-        scanf(" %d ", &opcion);  
-    } while (opcion == 1 );
-    mostrarLista(lista);
-    liberarLista(lista);
-    return 0;
