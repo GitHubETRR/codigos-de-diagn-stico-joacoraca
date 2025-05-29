1f65ad7266c1bbf3f5081e655749d5ec5e529835
joacoraca <114175765+joacoraca@users.noreply.github.com>
2025-05-29 10:43:01 -0300

    Update archivos.c

--- Codigos/archivos.c
+++ Codigos/archivos.c
@@ -9,14 +9,14 @@ int main() {
         printf("No se puede crear el archivo.\n");
         return 1;
     }
-    fprintf(archivo, "Hola, este es un archivo simple\n");
+    fprintf(archivo, "Hola, este es mi primer archivo\n");
     fclose(archivo);
     archivo = fopen("archivo.txt", "r");
     if (archivo == NULL) {
         printf("No se pudo abrio bien el archivo\n");
         return 1;
     }
-    printf("Contenido del archivo:\n");
+    printf("Contenido:\n");
     while (fgets(texto, sizeof(texto), archivo)) {
         printf("%s", texto);
     }
